56d724ea107fe56995d27f25c88e175b8b9f0c75
Varinder Kaur <VK92@myscc.ca>
2023-02-01 12:10:00 -0500

    updated files

---
+++ Var_Kaur_GroceryMarket/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Var_Kaur_GroceryMarket';
+  name: string='Varinder'
+}
---
+++ Var_Kaur_GroceryMarket/src/app/helper-files/content-interface.ts
@@ -0,0 +1,12 @@
+export interface Content {
+    id: number;
+    title : string;
+    description : string;
+    creator : string;
+    imagelink? : string;
+    type? : string;
+    hashtags?: string[];
+
+
+}
+
