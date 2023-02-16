3b292b8e29a70a162dd006f9ef7b805d065f0282
Varinder Kaur <VK92@myscc.ca>
2023-02-16 16:20:23 -0500

    updated the file

--- Var_Kaur_GroceryMarket/src/app/app.component.ts
+++ Var_Kaur_GroceryMarket/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
   title = 'Var_Kaur_GroceryMarket';
 }
