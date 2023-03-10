ff1e902b24e9c627bed814796c8840862eab6f5d
Varinder Kaur <VK92@myscc.ca>
2023-03-09 19:22:04 -0500

    deleted

--- Var_Kaur_GroceryMarket/src/app/filter-with-content.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterWithContentPipe } from './filter-with-content.pipe';
-
-describe('FilterWithContentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterWithContentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- Var_Kaur_GroceryMarket/src/app/filter-with-type.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterWithTypePipe } from './filter-with-type.pipe';
-
-describe('FilterWithTypePipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterWithTypePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
